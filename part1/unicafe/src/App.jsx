85b8f7c038bc6428ee252648df2a3b155eba4300
Rohitmajalatti05 <141742203+Rohitmajalatti05@users.noreply.github.com>
2024-09-06 17:44:55 +0530

    1.7: unicafe step 2 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -20,6 +20,9 @@ const Statistics = (props) => {
     <p>good {props.good}</p>
     <p>neutral {props.neutral}</p>
     <p>bad {props.bad}</p>
+    <p>all {props.all}</p>
+    <p>average {props.average}</p>
+    <p>positive {props.percentage} %</p>
   </div>
  )
 }
@@ -30,6 +33,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const percentage = (good * 100) / all;
 
   return (
     <div>
@@ -37,7 +43,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text ="good"/>
       <Button handleClick={() => setNeutral(neutral + 1)} text ="neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text ="bad"/>
-      <Statistics good = {good}  neutral = {neutral} bad = {bad}/>
+      <Statistics good = {good}  neutral = {neutral} bad = {bad} all= {all} average={average} percentage = {percentage}/>
      
     </div>
   )
