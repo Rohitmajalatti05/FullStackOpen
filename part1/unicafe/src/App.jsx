7dd7976fdf990b76843e56063fb9f01d0995f0c3
Rohitmajalatti05 <141742203+Rohitmajalatti05@users.noreply.github.com>
2024-09-06 18:11:31 +0530

    1.9: unicafe step 4 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,8 +13,11 @@ const Button = (props) =>{
     </div>
   )
 }
-// I have already created the Statistics component from the beginning.
+
 const Statistics = (props) => {
+  if (!(props.good || props.neutral || props.bad)) {
+    return <p>No feedback given</p>
+  }
  return(
   <div>
     <h2>Statistics</h2>
